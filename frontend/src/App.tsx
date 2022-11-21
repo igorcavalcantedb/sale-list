5fa2d350c121b66affd60fecd3501298a6c888dd
Igor Cavalcante <igor.brito@ilia.digital>
2022-11-21 18:30:45 +1300

    [Frontend] - WIP Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import "react-datepicker/dist/react-datepicker.css"
 import './App.css'
 import Header from './components/Header'
 import SalesCard from './components/SalesCard'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
 
   return (
     <div className="App">
+      <ToastContainer />
       <Header />
 
       <h1>Sale List</h1>
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,17 +1,25 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/notification-icon.svg'
 import { BASE_URL } from '../../util/request';
 type Props = {
-  saleId : number;
+  saleId: number;
 }
-function handleClick(id: number){
+function handleClick(id: number) {
   console.log("ID da venda =" + id);
   axios.post(`${BASE_URL}/v1/sales/notification`)
+    .then(
+      response => {
+        
+        toast.info("sms sent successfully!");
+
+      }
+    );
 }
 
-function NotificationButton({saleId} : Props) {
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn-container" onClick={()=> handleClick(saleId)}>
+    <div className="dsmeta-red-btn-container" onClick={() => handleClick(saleId)}>
       <div className="dsmeta-red-btn">
         <img src={icon} alt="Notificar" />
       </div>
